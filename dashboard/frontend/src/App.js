efdc4404f21807458627036f7d0de7c03d3877c5
AschalewMathewosDamtew <aschalewthecoder@gmail.com>
2024-11-04 15:27:14 +0300

    dashboard boilerplate

---
+++ dashboard/frontend/src/App.js
@@ -0,0 +1,14 @@
+// frontend/src/App.js
+
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Brent Oil Price Dashboard</h1>
+            <p>Explore the analysis results here.</p>
+        </div>
+    );
+}
+
+export default App;
